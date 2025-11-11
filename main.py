e1c6f0e25676db846ab68cb384a9944217da0162
oliver <ollyeh01@gmail.com>
2025-11-11 17:55:53 +0100

    Tested functions

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from pymatgen.io.cif import CifParser
-import package.fg
+from package import finding_generators as fg
+
 parser = CifParser("rocksalt_alnit.cif")
 
 structures = parser.parse_structures(primitive=False)
-atoms_dict = {}
+structure_dict = {}
 for i, site in enumerate(structures[0]*(1,1,1)):
     site = site.as_dict()
-    print(site)
-    atoms_dict[i] = {}
-    atoms_dict[i] = {"element": site["species"][0]["element"], "frac_coord": site["abc"]}
+    structure_dict[i] = {}
+    structure_dict[i] = {"element": site["species"][0]["element"], "frac_coord": site["abc"]}
+
+framework = fg.Framework(structure_dict, crystal_system="cubic")
+framework.dope_cell(4, "Sc")
+framework.display_cell()
