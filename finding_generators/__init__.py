71c7d56cae476bd8718aaaeee7367937e78a26c6
oliver <ollyeh01@gmail.com>
2025-12-04 13:27:53 +0100

    Rename

--- finding_generators/__init__.py
+++ finding_generators/__init__.py
@@ -2,7 +2,7 @@ from importlib.resources import files
 from .finding_generators import set_resource_path
 
 def get_easy3d_install_path():
-    path = str(files("FindingGenerators") / "easy3d" / "resources")
+    path = str(files("finding_generators") / "easy3d" / "resources")
     set_resource_path(path)
     return print(f"From __init__.py: {path}")
 
