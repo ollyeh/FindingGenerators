a02f846797e7993f87b656015f2caf9eda036b58
oliver <ollyeh01@gmail.com>
2025-12-06 08:49:42 +0100

    Fixed path

--- testing/full_run.py
+++ testing/full_run.py
@@ -1,8 +1,8 @@
 from finding_generators import AtomPermutator, GeneratorFinder
 
 if __name__ == "__main__":
-    ap = AtomPermutator("../rocksalt_alnit.cif", (2,2,2))
-    ap.dope_cell(5, "Mo")
+    ap = AtomPermutator("rocksalt_alnit.cif", (20,20,20))
+    ap.dope_cell(5, "Sc")
     ap.run_permutation("all_configurations.json")
     gf = GeneratorFinder("all_configurations.json", "Fm-3m")
     gf.start_reduction("ired_configurations.json")
